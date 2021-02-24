6c911f081b50cfbc9806b27f2646832f10e28be5
Kalin Roussinov <kalinrouss@gmail.com>
2021-02-24 13:11:54 +0200

    working on the TaskServcie

--- TaskService.js
+++ TaskService.js
@@ -9,7 +9,39 @@ const userStorage = (function () {
 
     class UserStorage {
         constructor() {
+            if (getItem('tasks')) {
+                this.tasks = getItem('tasks');
+            } else {
+                this.tasks = [];
+                setItem('tasks', this.tasks)
+            }
+        }
 
+        addTask(task, version) {
+            this.tasks = getItem('tasks');
+            this.tasks.push(new Task(task, version));
+            setItem('tasks', this.tasks);
+        }
+
+        removeTask(version) {
+            this.tasks = getItem('tasks');
+            this.tasks = this.tasks.filter(item => item.version === version);
+            setItem('tasks', this.tasks);
+        }
+
+        checkTask(version) {
+            this.tasks.array.forEach(task => {
+                if (task.uuid === version) {
+                    if (task.checked) {
+                        task.checked = false;
+                    } else {
+                        task.checked = true;
+                    }
+                    return setItem('tasks', this.tasks);
+                }
+            });
         }
     }
+
+    return new UserStorage();
 })();
--- utils.js
+++ utils.js
@@ -10,3 +10,19 @@ function getItem(key) {
 function setItem(key, value) {
     localStorage.setItem(key, JSON.stringify(value));
 }
+
+function print() {
+    const toDoList = getItem('tasks');
+    const source = getElementById('source').innerHTML;
+    const content = document.getElementById('tasksContainer');
+    const template = Handlebars.compile(source);
+    const html = template(toDoList);
+
+    content.innerHTML = html;
+
+    if (toDoList.length > 0) {
+        clearBtn.style.display = 'block';
+    } else {
+        clearBtn.style.display = 'none';
+    }
+}
